69e5e98ea72c3031cdbce885845cc2c3b73c0a94
Aniketgupta101 <aniketg266@gmail.com>
2024-09-23 20:52:18 +0530

    port

--- Web app/Backend/server.js
+++ Web app/Backend/server.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(5000, () => {
-    console.log("server started at http://localhost:5000")
+    console.log("server started at http://localhost:5000 hello")
 });
